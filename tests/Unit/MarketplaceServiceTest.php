5ab6d17a9995510eea398e41061c70df88274199
Abner Tudtud <abner@enjin.io>
2023-07-18 11:16:00 +0800

    fix test

--- tests/Unit/MarketplaceServiceTest.php
+++ tests/Unit/MarketplaceServiceTest.php
@@ -14,6 +14,7 @@ class MarketplaceServiceTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
+        $this->artisan('migrate:fresh');
         $this->createCollectionData();
     }
 
