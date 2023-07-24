259a7c4625a6dbd75b0e556d8916464a1d70bbf9
Abner Tudtud <114082473+enjinabner@users.noreply.github.com>
2023-07-24 19:33:23 +0800

    Add validation for cancelled listing (#6)

--- lang/en/validation.php
+++ lang/en/validation.php
@@ -3,4 +3,5 @@
 return [
     'future_block'=> 'The :attribute must be at least :block.',
     'enough_token_supply' => 'The token supply is not enough.',
+    'listing_not_cancelled' => 'The listing is already cancelled.',
 ];
---
+++ src/Rules/ListingNotCancelled.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Enjin\Platform\Marketplace\Rules;
+
+use Closure;
+use Enjin\Platform\Marketplace\Enums\ListingState;
+use Enjin\Platform\Marketplace\Models\Laravel\MarketplaceListing;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class ListingNotCancelled implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if (!$listing = MarketplaceListing::where('listing_id', $value)->with('state')->first()) {
+            $fail('validation.exists')->translate();
+
+            return;
+        }
+
+        if ($listing->state?->state === ListingState::CANCELLED->name) {
+            $fail('enjin-platform-marketplace::validation.listing_not_cancelled')->translate();
+        }
+    }
+}
