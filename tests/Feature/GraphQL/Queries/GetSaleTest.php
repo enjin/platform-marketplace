d16a8223d1278e28099c0dd220947b35215df7a0
Leonardo Custodio <leonardo@enjin.io>
2024-06-12 08:47:56 -0300

    [PLA-1629] Fixes MarketplaceSale (#51)

--- tests/Feature/GraphQL/Queries/GetSaleTest.php
+++ tests/Feature/GraphQL/Queries/GetSaleTest.php
@@ -4,6 +4,7 @@ namespace Enjin\Platform\Marketplace\Tests\Feature\GraphQL\Queries;
 
 use Enjin\Platform\Marketplace\Tests\Feature\GraphQL\TestCaseGraphQL;
 use Enjin\Platform\Support\Hex;
+use Illuminate\Support\Facades\Schema;
 
 class GetSaleTest extends TestCaseGraphQL
 {
@@ -19,6 +20,23 @@ class GetSaleTest extends TestCaseGraphQL
             $this->method,
             ['id' => $listing->sales->first()->id]
         );
+
+        $this->assertNotEmpty($response);
+    }
+
+    public function test_it_can_get_a_sale_with_no_listing(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        $listing = $this->createListing();
+        $saleId = $listing->sales->first()->id;
+        $listing->delete();
+        Schema::enableForeignKeyConstraints();
+
+        $response = $this->graphql(
+            $this->method,
+            ['id' => $saleId]
+        );
+
         $this->assertNotEmpty($response);
     }
 
