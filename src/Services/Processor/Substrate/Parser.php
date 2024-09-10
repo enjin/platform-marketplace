68eb3017642bcffc3bc93aa167430f49097e4138
Leonardo Custodio <leonardo@enjin.io>
2024-09-09 22:53:02 -0300

    [PLA-1986] Fixes marketplace parsing (#64)

--- src/Services/Processor/Substrate/Parser.php
+++ src/Services/Processor/Substrate/Parser.php
@@ -2,10 +2,10 @@
 
 namespace Enjin\Platform\Marketplace\Services\Processor\Substrate;
 
-use Carbon\Carbon;
 use Closure;
 use Enjin\Platform\Marketplace\Enums\ListingState;
 use Enjin\Platform\Marketplace\Models\MarketplaceBid;
+use Enjin\Platform\Marketplace\Models\MarketplaceListing;
 use Enjin\Platform\Marketplace\Models\MarketplaceState;
 use Enjin\Platform\Marketplace\Services\MarketplaceService;
 use Enjin\Platform\Marketplace\Services\Processor\Substrate\Codec\Codec;
@@ -89,12 +89,11 @@ class Parser extends BaseParser
                 'start_block' => Arr::get($listingData, 'data.Auction.startBlock'),
                 'end_block' => Arr::get($listingData, 'data.Auction.endBlock'),
                 'amount_filled' => ($amountFilled = Arr::get($listingData, 'state.FixedPrice.amountFilled')) !== null ? gmp_strval($amountFilled) : null,
-                'created_at' => $now = Carbon::now(),
-                'updated_At' => $now,
             ];
         }
 
-        $this->marketplaceService->insert($insertData);
+        MarketplaceListing::upsert($insertData, uniqueBy: 'listing_chain_id');
+
         $this->marketplaceBids($insertBids);
         $this->marketplaceStates($insertStates);
     }
@@ -120,8 +119,6 @@ class Parser extends BaseParser
                 // If the listing is currently in the storage means it is active
                 'state' => ListingState::ACTIVE->name,
                 'height' => Arr::get($state, 'height'),
-                'created_at' => $now = Carbon::now(),
-                'updated_at' => $now,
             ];
         }
 
@@ -155,8 +152,6 @@ class Parser extends BaseParser
                 'price' => ($price = Arr::get($bid, 'listingData.state.Auction.highBid.price')) !== null ? gmp_strval($price) : null,
                 // There is no way to know this info from state we will default to auction started block
                 'height' => Arr::get($bid, 'listingData.creationBlock'),
-                'created_at' => $now = Carbon::now(),
-                'updated_at' => $now,
             ];
         }
 
