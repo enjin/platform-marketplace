336d8ab0836e981bf05270a8d130a71b4224540d
Leonardo Custodio <leonardo@custodio.me>
2025-03-18 08:14:21 -0300

    changes

--- src/GraphQL/Mutations/FillListingMutation.php
+++ src/GraphQL/Mutations/FillListingMutation.php
@@ -12,10 +12,8 @@ use Enjin\Platform\GraphQL\Types\Input\Substrate\Traits\HasIdempotencyField;
 use Enjin\Platform\GraphQL\Types\Input\Substrate\Traits\HasSigningAccountField;
 use Enjin\Platform\GraphQL\Types\Input\Substrate\Traits\HasSimulateField;
 use Enjin\Platform\Interfaces\PlatformBlockchainTransaction;
-use Enjin\Platform\Marketplace\Models\Laravel\MarketplaceListing;
 use Enjin\Platform\Marketplace\Rules\ListingNotCancelled;
-use Enjin\Platform\Models\Collection;
-use Enjin\Platform\Models\Token;
+use Facades\Enjin\Platform\Marketplace\Services\MarketplaceService;
 use Enjin\Platform\Models\Transaction;
 use Enjin\Platform\Rules\MaxBigInt;
 use Enjin\Platform\Rules\MinBigInt;
@@ -99,21 +97,12 @@ class FillListingMutation extends Mutation implements PlatformBlockchainTransact
     #[\Override]
     public static function getEncodableParams(...$params): array
     {
-        $listingId = Arr::get($params, 'listingId');
-        $royaltyCount = 0;
-        if (currentSpec() >= 1020) {
-            $listing = MarketplaceListing::firstWhere('id', $listingId);
-            $makeCollection = Collection::firstWhere('collection_chain_id', $listing?->make_collection_chain_id);
-            $makeToken = Token::firstWhere(['collection_id' => $makeCollection?->id, 'token_chain_id' => $listing?->make_token_chain_id]);
-            if ($makeCollection?->royalty_wallet_id !== null || $makeToken?->royalty_wallet_id !== null) {
-                $royaltyCount = 1;
-            }
-        }
+        $count = MarketplaceService::getRoyaltyBeneficiaryCount($listingId = Arr::get($params, 'listingId'));
 
         return [
             'listingId' => HexConverter::unPrefix($listingId),
             'amount' => gmp_init(Arr::get($params, 'amount', 0)),
-            'royaltyBeneficiaryCount' => $royaltyCount,
+            'royaltyBeneficiaryCount' => $count,
         ];
     }
 
--- src/Services/Processor/Substrate/Codec/Encoder.php
+++ src/Services/Processor/Substrate/Codec/Encoder.php
@@ -13,6 +13,7 @@ class Encoder extends BaseEncoder
         'FillListing' => 'Marketplace.fill_listing',
         'FillListingV1013' => 'Marketplace.fill_listing',
         'FinalizeAuction' => 'Marketplace.finalize_auction',
+        'FinalizeAuctionV1013' => 'Marketplace.finalize_auction',
         'PlaceBid' => 'Marketplace.place_bid',
         'SetProtocolFee' => 'Marketplace.set_protocol_fee',
     ];
